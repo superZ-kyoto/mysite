02e7674cb58d98cee9a7b6eb8feb543a6ef6d827
superZ <takumi.hondo@gmail.com>
2020-06-20 14:28:27 +0900

    test

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-a = input('底を入力してください')
-b = int(a)**int(a)**int(a)
-print (b)
+a = input('お兄さんの最終学歴教えてください')
+if a == '京都大学' :
+    print("京大、やるやん")
+elif a == '東京大学' :
+    print("東大、やるやん")
+else:
+    print ("Fランやないか")
